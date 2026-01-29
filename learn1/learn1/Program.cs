cd507f5878e315fa2b838903e404032cbc6c8cb4
Thai Truong Giang <truonggiang121604@gmail.com>
2026-01-29 08:11:32 +0700

    bai tap

---
+++ learn1/learn1/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Write("Nhap chuoi: ");
+String chuoi = Console.ReadLine();
+
+if (chuoi == null)
+{
+    Console.WriteLine("So tu: 0");
+    return;
+}
+chuoi = chuoi.Trim();
+
+if (chuoi.Length == 0)
+{
+    Console.WriteLine("So tu: 0");
+    return;
+}
+
+string[] words = chuoi.Split(
+    new char[] { ' ' },
+    StringSplitOptions.RemoveEmptyEntries
+);
+
+Console.WriteLine("So tu: " + words.Length);
---
+++ learn2/learn2/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.Write("Nhap chuoi: ");
+string s = Console.ReadLine();
+if (s == null)
+{
+    Console.WriteLine("Chuoi KHONG doi xung");
+    return;
+}
+s = s.Replace(" ", "");
+
+int left = 0;
+int right = s.Length - 1;
+bool doiXung = true;
+
+while (left < right)
+{
+    if (s[left] != s[right])
+    {
+        doiXung = false;
+        break;
+    }
+    left++;
+    right--;
+}
+
+if (doiXung)
+    Console.WriteLine("Chuoi doi xung");
+else
+    Console.WriteLine("Chuoi KHONG doi xung");
